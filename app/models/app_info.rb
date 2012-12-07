7e4dac305bd3a5198c7dee0ea118240fb76e15ce
Tomwey <tomwey@163.com>
2012-12-07 22:07:01 +0800

    merge

--- app/models/app_info.rb
+++ app/models/app_info.rb
@@ -1,9 +1,7 @@
 class AppInfo < ActiveRecord::Base
   attr_accessible :app_url, :description, :title, :image
   mount_uploader :image, ImageUploader
-<<<<<<< HEAD
   
   scope :recent, order('id DESC')
-=======
->>>>>>> b38ed4179a3d7bf2dd826a2ba337a6c83f029f19
+
 end
