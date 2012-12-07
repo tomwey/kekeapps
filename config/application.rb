88b85ec7225e073550f0810eacf05e4bef854215
Tomwey <tomwey@163.com>
2012-12-07 22:01:05 +0800

    update application.rb

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module Kekeapps
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :password_confirmation]
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
@@ -55,6 +55,7 @@ module Kekeapps
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
