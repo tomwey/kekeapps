4953529a8cf2ab4e366ed5b3c5759ba15f9df5dd
Tomwey <tomwey@163.com>
2012-12-12 00:26:53 +0800

    x

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(email:"kekestudio@163.com", password:"123456", password_confirmation:"123456")
