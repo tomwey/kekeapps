e6fde576a026da8a090f988368c5db6d66c8ac13
Tomwey <tomwey@163.com>
2012-12-15 16:37:00 +0800

    image not show

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,10 +25,10 @@ Kekeapps::Application.configure do
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
   
   # Heroku
-  config.action_dispatch.x_sendfile_header = nil
+  # config.action_dispatch.x_sendfile_header = nil
   
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
