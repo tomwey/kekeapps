bd75562f1d6e2380e5cb55254a743108a794fa69
Tomwey <tomwey@163.com>
2013-01-17 23:05:39 +0800

    fix category bug

--- app/models/law_category.rb
+++ app/models/law_category.rb
@@ -2,4 +2,9 @@ class LawCategory < ActiveRecord::Base
   attr_accessible :description, :name, :visible
   
   has_many :law_datas, :foreign_key => "parent_id"
+  
+  def self.category_collection
+    @categories ||= LawCategory.all
+    @categories.collect { |c| [c.name, c.id] }
+  end
 end
