72a3cbdad3f021863cc76712e02992b66b5297b6
Tomwey <tomwey@163.com>
2013-03-28 00:38:28 +0800

    xxx

---
+++ db/migrate/20130427142338_add_law_details_count.rb
@@ -0,0 +1,14 @@
+class AddLawDetailsCount < ActiveRecord::Migration
+  def up
+    add_column :app_infos, :law_details_count, :integer, :default => 0
+    
+    AppInfo.reset_column_information
+    AppInfo.find_each do |info|
+      info.reset_counters info.id, :law_details
+    end
+  end
+
+  def down
+    remove_column :app_infos, :law_details_count
+  end
+end
