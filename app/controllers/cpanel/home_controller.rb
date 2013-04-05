7b98bfd74ce05e362f96b3e99af1fcfed58e47a4
Tomwey <tomwey@163.com>
2013-04-05 22:08:05 +0800

    update home

--- app/controllers/cpanel/home_controller.rb
+++ app/controllers/cpanel/home_controller.rb
@@ -1,7 +1,13 @@
 class Cpanel::HomeController < Cpanel::ApplicationController
   def index
-    @recent_app_infos = AppInfo.recent.limit(20)
-    @recent_news_blasts = NewsBlast.recent.limit(20)
-    @recent_feedbacks = Feedback.recent.limit(20)
+    @recent_app_infos = AppInfo.recent.limit(10)
+    @recent_feedbacks = Feedback.recent.limit(10)
+    
+    app_info = @recent_app_infos.first
+    feedback = @recent_feedbacks.first
+    
+    fresh_when :etag => app_info.hash + feedback.hash
+    
   end
+  
 end
